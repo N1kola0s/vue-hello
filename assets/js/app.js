b4ea4bba335d88f90c49aff67bf334f3102e66a8
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-04-20 15:50:09 +0200

    add h1 and img in Dom with vue.js

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+const app = new Vue (
+    {
+
+        el:'#app',
+        
+        data: {
+
+            message: 'Hello Vue',
+            url_img: './assets/img/laptop.webp',
+            txt_img:'laptop'
+
+
+        },
+
+
+
+    }
+)
